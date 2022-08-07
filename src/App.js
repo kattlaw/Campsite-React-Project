cee1cc81f1633a5f555cbfbf2c63c24e20ec4fd1
Katherine Law <katherine.law2@gmail.com>
2022-08-07 16:28:24 -0400

    Workshop 2 updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
           <Route path='contact' element={<ContactPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />} />
           <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+          <Route path='about' element={<AboutPage />} />
         </Routes>
         <Footer />
       </div>
