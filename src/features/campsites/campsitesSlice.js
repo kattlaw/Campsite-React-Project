f776dee7bdc6f6559986c1ad214f46e2c126e9ad
Katherine Law <katherine.law2@gmail.com>
2022-08-08 12:47:17 -0400

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
